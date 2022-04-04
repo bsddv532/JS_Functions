358c8eb8ad4b8e46484fba5185f77ce1eac34e59
bsddv532 <deepakpradhan9962@gmail.com>
2022-04-04 13:48:18 +0530

    JAVASCRIPT FUNCTIONS

---
+++ index.js
@@ -0,0 +1,19 @@
+// Function without parameters, without return.
+function add() {
+    console.log('Hi')
+}
+add();  //Hi
+
+
+// Function with params, without return.
+function name(userName) {
+    console.log(userName);
+}
+name('Deepak'); //Chetan
+
+
+// Function with params, with return.
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(3, 5)); //15
